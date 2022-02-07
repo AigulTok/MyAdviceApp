5a2cda16d692cfd14833e22cd673ac2915e03347
Токтасынова Айгуль <a.toktassynova@medservice.kz>
2022-02-07 22:31:55 +0600

    Добавила ссылку в /author

--- main.py
+++ main.py
@@ -19,25 +19,11 @@ def hello_page(request: Request):
     return templates.TemplateResponse("item.html", hello_data)
 
 
-# @app.get("/", response_class=HTMLResponse)
-# def hello_page(request: Request):
-#     hello_data = {
-#             "request": request}
-#     return templates.TemplateResponse("index.html", hello_data)
-
-
 @app.get("/about", response_class=HTMLResponse)
 def my_about(request: Request):
     about_data = {
         "request": request}
     return templates.TemplateResponse("about.html", about_data)
-
-
-# @app.get("/about/", response_class=HTMLResponse)
-# def my_about(request: Request):
-#     about_data = {
-#         "request": request}
-#     return templates.TemplateResponse("index.html", about_data)
 
 
 @app.get("/project", response_class=HTMLResponse)
@@ -50,30 +36,12 @@ def my_advice(request: Request):
         result = response.json()
         data = {
             "request": request,
-            "result": result['slip']['advice']
+            "result": result["slip"]["advice"]
         }
 
         return templates.TemplateResponse("project.html", data)
     else:
         return 'Хватит слушать чужие советы, думай своей головой;)'
-
-
-# @app.get("/project", response_class=HTMLResponse)
-# def my_advice(request: Request):
-#     url = 'https://api.adviceslip.com/advice'
-#
-#     response = requests.get(url)
-#
-#     if response.status_code == 200:
-#         result = response.json()
-#         data = {
-#             "request": request,
-#             "result": result['slip']['advice']
-#         }
-#
-#         return templates.TemplateResponse("index.html", data)
-#     else:
-#         return 'Хватит слушать чужие советы, думай своей головой;)'
 
 
 @app.get("/author", response_class=HTMLResponse)
